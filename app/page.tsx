f22ea93902cda0b427f892b4be652b6e9e7caab8
Kristof Gazso <kristof.gazso@gmail.com>
2024-02-04 12:46:11 +0000

    up

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Send Transaction'
+      label: 'Deploy Smart Account'
     }
   ],
   image: `${NEXT_PUBLIC_URL}/main.png`,
