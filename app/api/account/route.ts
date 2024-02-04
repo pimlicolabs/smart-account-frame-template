b3d22c6b2d65d522dccbc1a2a8076b73ec298284
Kristof Gazso <kristof.gazso@gmail.com>
2024-02-04 12:02:24 +0000

    up

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -1,11 +1,7 @@
 import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
-import { bundlerActions, createSmartAccountClient } from 'permissionless';
-import { privateKeyToSafeSmartAccount } from 'permissionless/accounts';
-import { pimlicoBundlerActions } from 'permissionless/actions/pimlico';
 import { createPimlicoPaymasterClient } from 'permissionless/clients/pimlico';
-import { Address, createPublicClient, hexToBigInt, http } from 'viem';
-import { sepolia } from 'viem/chains';
+import { createPublicClient, http } from 'viem';
 
 const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
 
@@ -34,37 +30,37 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         return new NextResponse('Invalid Frame message', { status: 400 });
     }
 
-    const accountAddress = message.interactor.verified_accounts[0] as Address;
+    // const accountAddress = message.interactor.verified_accounts[0] as Address;
 
-    // send transaction
-    const account = await privateKeyToSafeSmartAccount(publicClient, {
-        privateKey: privateKey as Address,
-        safeVersion: "1.4.1", // simple version
-        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
-        saltNonce: hexToBigInt(accountAddress)
-    })
+    // // send transaction
+    // const account = await privateKeyToSafeSmartAccount(publicClient, {
+    //     privateKey: privateKey as Address,
+    //     safeVersion: "1.4.1", // simple version
+    //     entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
+    //     saltNonce: hexToBigInt(accountAddress)
+    // })
 
-    const smartAccountClient = createSmartAccountClient({
-        account,
-        chain: sepolia,
-        transport: http(bundlerUrl),
-        sponsorUserOperation: paymasterClient.sponsorUserOperation,
-    })
-        .extend(bundlerActions)
-        .extend(pimlicoBundlerActions)
+    // const smartAccountClient = createSmartAccountClient({
+    //     account,
+    //     chain: sepolia,
+    //     transport: http(bundlerUrl),
+    //     sponsorUserOperation: paymasterClient.sponsorUserOperation,
+    // })
+    //     .extend(bundlerActions)
+    //     .extend(pimlicoBundlerActions)
         
-    const gasPrices = await smartAccountClient.getUserOperationGasPrice()    
+    // const gasPrices = await smartAccountClient.getUserOperationGasPrice()    
     
-    console.log(gasPrices)
+    // console.log(gasPrices)
 
-    smartAccountClient.sendTransaction({
-        to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-        data: "0x1234",
-        maxFeePerGas: gasPrices.fast.maxFeePerGas,
-        maxPriorityFeePerGas: gasPrices.fast.maxPriorityFeePerGas,
-    })
+    // smartAccountClient.sendTransaction({
+    //     to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+    //     data: "0x1234",
+    //     maxFeePerGas: gasPrices.fast.maxFeePerGas,
+    //     maxPriorityFeePerGas: gasPrices.fast.maxPriorityFeePerGas,
+    // })
 
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    // await new Promise((resolve) => setTimeout(resolve, 1000))
 
     return new NextResponse(
         getFrameHtmlResponse({
