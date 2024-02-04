56b84eeb977befc6337d017d0b200f083b1186fc
Kristof Gazso <kristof.gazso@gmail.com>
2024-02-04 01:03:16 +0000

    lol

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -22,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         getFrameHtmlResponse({
             buttons: [
                 {
-                    label: `Text`,
+                    label: `Text ${message?.interactor.custody_address}`,
                 },
             ],
             image: `${NEXT_PUBLIC_URL}/park-2.png`,
