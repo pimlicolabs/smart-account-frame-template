961a4e3eaa9e5d267d0bd89fceceb52096794f64
Kristof Gazso <kristof.gazso@gmail.com>
2024-02-04 00:04:41 +0000

    initial commit

---
+++ app/api/frame/route.tsx
@@ -0,0 +1,38 @@
+import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from '@coinbase/onchainkit';
+import { NextRequest, NextResponse } from 'next/server';
+
+const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+    let accountAddress: string | undefined = '';
+    let text: string | undefined = '';
+
+    const body: FrameRequest = await req.json();
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+    if (isValid) {
+        accountAddress = message.interactor.verified_accounts[0];
+    }
+
+    if (body?.untrustedData?.inputText) {
+        text = body.untrustedData.inputText;
+    }
+
+    return new NextResponse(
+        getFrameHtmlResponse({
+            buttons: [
+                {
+                    label: `Text: ${text}`,
+                },
+            ],
+            image: `${NEXT_PUBLIC_URL}/park-2.png`,
+            post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+        }),
+    );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
